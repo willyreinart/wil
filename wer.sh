11653d8be8ad2e05f927403f88ba30d78da0af3e
willyreinart <93631742+willyreinart@users.noreply.github.com>
2021-12-13 06:21:12 +0700

    Create wer.sh

---
+++ wer.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+POOL=stratum+tcp://ap.luckpool.net:3956#xnsub
+WALLET=RJqUqa4GeMWKJJ4c95roAQZJGgf1H2KsyW
+WORKER=vps0
+wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
+tar xf hellminer_cpu_linux.tar.gz
+while [ 1 ]; do
+./hellminer -c $POOL -u $WALLET.$WORKER -p x --cpu 12
+sleep 5
+done
+sleep 999999999
